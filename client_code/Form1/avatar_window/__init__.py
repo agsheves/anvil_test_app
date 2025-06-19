16d1e7ebdc5f4e5a9e32c85f4dac37a0bd62b2d6
andrew@tarjumansolutions.com <andrew@tarjumansolutions.com>
2025-06-19 20:32:37 +0000

    Created ServerModule1 and Form1.avatar_window; Edited settings and Form1

--- client_code/Form1/avatar_window/__init__.py
+++ client_code/Form1/avatar_window/__init__.py
@@ -1,14 +1,14 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import avatar_windowTemplate
 from anvil import *
 import anvil.server
+import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.users
 
-
-class Form1(Form1Template):
+class avatar_window(avatar_windowTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    #anvil.users.login_with_form()# Any code you write here will run before the form opens.
+
+    # Any code you write here will run before the form opens.
